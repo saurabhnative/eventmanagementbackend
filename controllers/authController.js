d34ed61e631dbc02bba35f64d0919823178e8fa9
Saurabh Mhatre <codeclassifiers@gmail.com>
2023-06-24 02:00:00 +0530

    Auth fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 
 const authController = {
   register: async (req, res) => {
